1faaebbf59f06dd2412fe64828406038caf5d9b7
haywoodeee <bprescott@rightpoint.com>
2016-10-12 15:52:47 -0500

    Add files via upload

---
+++ Install-DSCResources.ps1
@@ -0,0 +1,19 @@
+﻿new-item "c:\temp2" -type Directory
+$url = "https://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/131371/4/DSC%20Resource%20Kit%20Wave%2010%2004012015.zip"
+$outputPath = "C:\temp2\dscresource.zip"
+
+Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+function Expand-ZIPFile($file, $destination)
+{
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items())
+{
+$shell.Namespace($destination).copyhere($item)
+}
+}
+
+Expand-ZIPFile -file $outputPath -destination "c:\program files\WindowsPowerShell\Modules\"
