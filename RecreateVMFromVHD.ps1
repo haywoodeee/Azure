fe2f94f86d3b6df6422ff9e5d45062d8730badba
haywoodeee <bprescott@rightpoint.com>
2017-01-27 14:26:53 -0600

    Add files via upload

---
+++ RecreateVMFromVHD.ps1
@@ -0,0 +1,18 @@
+﻿
+$osDiskUri = "https://myqppstor1.blob.core.windows.net/vhds/MyQ-PP-DC120161017132258.vhd"
+$RGname = Read-Host -Prompt "Enter the resource group name"
+$vmName = Read-Host -Prompt "Enter the VM name"
+$vmNIC = Read-Host -Prompt "Enter the VM NIC name"
+$vmSize = Read-Host -Prompt "Enter the VM size"
+
+$location = "North Central US"
+
+
+$nic = Get-AzureRmNetworkInterface -Name $vmNIC -ResourceGroupName $RGname
+
+$vmConfig = New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize
+$vmConfig = Set-AzureRmVMOSDisk -VM $vmConfig -Name $vmName -VhdUri $osDiskUri -CreateOption Attach -Windows
+$vmConfig = Add-AzureRmVMNetworkInterface -VM $vmConfig -Id $nic.Id
+
+$vm = New-AzureRmVM -VM $vmConfig -Location $location -ResourceGroupName $RGname
+
