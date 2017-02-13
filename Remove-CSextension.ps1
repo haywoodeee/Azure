0dd05430dabb54fd576a541f1691d99039afa233
haywoodeee <bprescott@rightpoint.com>
2017-02-13 08:51:32 -0600

    Add files via upload

---
+++ Remove-CSextension.ps1
@@ -0,0 +1,10 @@
+﻿Login-AzureRmAccount
+
+$ResourceGroupName = "MyQ-PP-WEB-RG"
+$customscriptname = "octopusinstall"
+
+$nodes = Get-AzureRmVM -ResourceGroupName $ResourceGroupName
+
+$nodes | % {Remove-AzurermVMCustomScriptExtension -ResourceGroupName $ResourceGroupName -VMName $_.name –Name $customscriptname -Force}
+
+
